11575a5ccc1d26662d67ee3487c004bb2b737138
prabhat <prabhat.singh_cs18@gla.ac.in>
2020-08-27 12:56:55 -0700

    Creating a reference Class

--- app/src/main/java/com/example/safecabapp/MapActivityReference.java
+++ app/src/main/java/com/example/safecabapp/MapActivityReference.java
@@ -3,12 +3,24 @@ package com.example.safecabapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class MapActivityReference extends AppCompatActivity {
+    private Button mGetStarted;
+    private EditText mEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_reference);
+        mGetStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mEdit = "sanal"
+            }
+        });
     }
 }
