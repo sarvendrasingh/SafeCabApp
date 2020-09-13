1a93c53952b945fc91bad118b9d356771ed98de1
prabhat <prabhat.singh_cs18@gla.ac.in>
2020-09-12 17:46:36 -0700

    PayPalConfig: new java file

---
+++ app/src/main/java/com/example/safecabapp/PayPalConfig.java
@@ -0,0 +1,7 @@
+package com.example.safecabapp;
+
+public class PayPalConfig {
+    public static final String PAYPAL_CLIENT_ID = "AY1gJsu5ioVnOPTP4kQumV1J0hqSPaIrZIrn3vP88Nc8kW0xKFG7H7KE2rzrWr7Lk9ca-7XJCUkRZFpC";
+
+
+}
